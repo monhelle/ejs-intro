a071c46d40556ba92f46bfaeb9046261e1ea600b
Monica <monhelle@gmail.com>
2026-05-26 14:47:43 +0200

    adding models and connecting to mongodb

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 const express = require("express");
 const argon2 = require("argon2");
+const mongodb = require("mongoose");
 
+const User = require("./models/User");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongodb.connect("mongodb://localhost:27017/ejs"); //notat
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 app.get("/registrer", (req, res) => {
-    res.render("registrer")
-})
+  res.render("registrer");
+});
 app.post("/", (req, res) => {
-    const { email, passord } = req.body;
-    console.log(req.body)
+  const { email, passord } = req.body;
+  console.log(req.body);
 
-    res.send("OK")
-    
-})
+  res.send("OK");
+});
 app.post("/registrer", async (req, res) => {
-    const { email, passord, gjentaPassord } = req.body;
-    console.log(req.body)
-    if(passord === gjentaPassord) {
-        const hash = await argon2.hash(passord);
-        console.log(hash)
+  const { email, passord, gjentaPassord } = req.body;
+  console.log(req.body);
+  if (passord === gjentaPassord) {
+    const hash = await argon2.hash(passord);
+    console.log(hash);
 
-        res.send("OK " + hash)
-    } else {
-        res.send("Passord og gjenta passord matcher ikke")
-    }
-    
-})
+    const user = new User({ 
+        email: email, 
+        passord: hash 
+    });
+
+    await user.save();
+    res.redirect("/");
+    // res.send("OK " + hash);
+  } else {
+    res.send("Passord og gjenta passord matcher ikke");
+  }
+});
 app.listen(4000);
